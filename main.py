a208ae5787633a6e69e211073f5328d3ea943bdd
Niskha <112144442+Niskha@users.noreply.github.com>
2024-02-29 02:52:30 -0500

    Initial Code, Not full project file
    using kivy to package as an app, the full project folder is larger but ultimately meaningless for now.

---
+++ main.py
@@ -0,0 +1,72 @@
+'''
+Camera Example
+==============
+
+This example demonstrates a simple use of the camera. It shows a window with
+a buttoned labelled 'play' to turn the camera on and off. Note that
+not finding a camera, perhaps because gstreamer is not installed, will
+throw an exception during the kv language processing.
+
+'''
+
+# Uncomment these lines to see all the messages
+# from kivy.logger import Logger
+# import logging
+# Logger.setLevel(logging.TRACE)
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+import time
+import cv2
+from kivy.properties import StringProperty
+Builder.load_string('''
+<CameraClick>:
+    orientation: 'vertical'
+    Camera:
+        id: camera
+        resolution: (640, 480)
+        play: False
+    ToggleButton:
+        text: 'Play'
+        on_press: camera.play = not camera.play
+        size_hint_y: None
+        height: '48dp'
+    Button:
+        text: 'Capture'
+        size_hint_y: None
+        height: '48dp'
+        on_press: root.capture()
+    Label:
+        id: label1
+        text: root.data
+        height: '12dp'
+''')
+
+
+class CameraClick(BoxLayout):
+    data = StringProperty()
+
+    def capture(self):
+        '''
+        Function to capture the images and give them the names
+        according to their captured time and date.
+        '''
+        camera = self.ids['camera']
+        timestr = time.strftime("%Y%m%d_%H%M%S")
+        camera.export_to_png("IMG_{}.png".format(timestr))
+        detect = cv2.QRCodeDetector()
+        img = cv2.imread("IMG_{}.png".format(timestr))
+        data = detect.detectAndDecode(img)
+        self.data = data[0]
+        print("Captured")
+        print(self.data)
+
+
+class TestCamera(App):
+
+    def build(self):
+        return CameraClick()
+
+
+TestCamera().run()
